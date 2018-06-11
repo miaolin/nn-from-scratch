9e9af1888ff694be915587d67feddd4b8a31646e
oaimnil <miao.lin@teralytics.ch>
2018-06-11 11:56:50 +0800

    remove unused code

--- nn_example.py
+++ nn_example.py
@@ -1,10 +1,45 @@
 import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
 
-from simple_classification import generate_data
 
-# Gradient descent parameters (I picked these by hand)
-epsilon = 0.01  # learning rate for gradient descent
-reg_lambda = 0.01  # regularization strength
+class nn_config:
+    nn_input_dim = 2  # input layer dimensionality
+    nn_output_dim = 2  # output layer dimensionality
+    # Gradient descent parameters (I picked these by hand)
+    epsilon = 0.01  # learning rate for gradient descent
+    reg_lambda = 0.01  # regularization strength
+
+
+def generate_data(sample_num):
+    np.random.seed(0)
+    X, y = datasets.make_moons(sample_num, noise=0.20)
+    return X, y
+
+
+def visualize(X, y, clf=None):
+    if not clf:
+        plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
+        plt.show()
+    else:
+        plot_decision_boundary(lambda x: clf.predict(x), X, y)
+        plt.title("Logistic Regression")
+
+
+def plot_decision_boundary(pred_func, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole gid
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+    plt.show()
 
 
 def calculate_loss(X, y, model):
@@ -27,7 +62,7 @@ def calculate_loss(X, y, model):
     data_loss = np.sum(corect_logprobs)
 
     # Add regulatization term to loss (optional)
-    data_loss += reg_lambda/ 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+    data_loss += nn_config.reg_lambda / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
     return 1. / num_examples * data_loss
 
 
@@ -60,16 +95,16 @@ def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
     :return:
     """
 
-    nn_input_dim = 2  # input layer dimensionality
-    nn_output_dim = 2  # output layer dimensionality
+    #nn_input_dim = 2  # input layer dimensionality
+    #nn_output_dim = 2  # output layer dimensionality
     num_examples = len(X)
 
     # Initialize the parameters to random values. We need to learn these.
     np.random.seed(0)
-    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+    W1 = np.random.randn(nn_config.nn_input_dim, nn_hdim) / np.sqrt(nn_config.nn_input_dim)
     b1 = np.zeros((1, nn_hdim))
-    W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
-    b2 = np.zeros((1, nn_output_dim))
+    W2 = np.random.randn(nn_hdim, nn_config.nn_output_dim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, nn_config.nn_output_dim))
 
     # This is what we return at the end
     model = {}
@@ -87,21 +122,21 @@ def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
         # Backpropagation
         delta3 = probs
         delta3[range(num_examples), y] -= 1
-        dW2 = (a1.T).dot(delta3)
+        dW2 = a1.T.dot(delta3)
         db2 = np.sum(delta3, axis=0, keepdims=True)
         delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
         dW1 = np.dot(X.T, delta2)
         db1 = np.sum(delta2, axis=0)
 
         # Add regularization terms (b1 and b2 don't have regularization terms)
-        dW2 += reg_lambda * W2
-        dW1 += reg_lambda * W1
+        dW2 += nn_config.reg_lambda * W2
+        dW1 += nn_config.reg_lambda * W1
 
         # Gradient descent parameter update
-        W1 += -epsilon * dW1
-        b1 += -epsilon * db1
-        W2 += -epsilon * dW2
-        b2 += -epsilon * db2
+        W1 += -nn_config.epsilon * dW1
+        b1 += -nn_config.epsilon * db1
+        W2 += -nn_config.epsilon * dW2
+        b2 += -nn_config.epsilon * db2
 
         # Assign new parameters to the model
         model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
@@ -114,15 +149,27 @@ def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
     return model
 
 
+def evaluation(X, y):
+    plt.figure(figsize=(16, 32))
+    hidden_layer_dimensions = [1, 2, 3, 4, 5]
+    for i, nn_hdim in enumerate(hidden_layer_dimensions):
+        plt.subplot(5, 2, i + 1)
+        plt.title('Hidden Layer size %d' % nn_hdim)
+        model = build_model(X, y, nn_hdim, True)
+        plot_decision_boundary(lambda x: predict(model, x), X, y)
+    plt.show()
+
+
 def main():
 
-    sample_num = 20000
+    sample_num = 1000
     X, y = generate_data(sample_num)
 
     # Build a model with a 3-dimensional hidden layer
-    nn_hidden_num = 3
-    model = build_model(X, y, nn_hidden_num, print_loss=True)
+    #nn_hidden_num = 3
+    #model = build_model(X, y, nn_hidden_num, print_loss=True)
 
+    evaluation(X, y)
 
 if __name__ == "__main__":
     main()
